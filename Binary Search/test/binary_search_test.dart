c96680fc0a35e46b4391bf4234ea870702d13eb0
Jakub Sekerka <sekerka.jakub@gmail.com>
2019-08-26 18:46:46 +0200

    Separate binary search tests into groups based on the num type

--- Binary Search/test/binary_search_test.dart
+++ Binary Search/test/binary_search_test.dart
@@ -3,44 +3,48 @@ import 'package:test/test.dart';
 import '../lib/main.dart';
 
 void main() {
-  test('Searching for int value included in the list', () {
-    final numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67];
+  group('int', () {
+    test('Searching for value included in the list', () {
+      final numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67];
 
-    final positionInTheList = binarySearch(numbers, key: 43, lowerRange: 0, upperRange: numbers.length);
+      final positionInTheList = binarySearch(numbers, key: 43, lowerRange: 0, upperRange: numbers.length);
 
-    expect(positionInTheList, equals(13));
+      expect(positionInTheList, equals(13));
+    });
+
+    test('Searching for value not included in the list', () {
+      final numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67];
+
+      final index = binarySearch(numbers, key: 10, lowerRange: 0, upperRange: numbers.length);
+
+      expect(index, isNull);
+    });
+
+    test('Searching for value in one-item list', () {
+      final numbers = [1];
+
+      final index = binarySearch(numbers, key: 1, lowerRange: 0, upperRange: numbers.length);
+
+      expect(index, equals(0));
+    });
+
+    test('Searching for value in an empty list', () {
+      final numbers = <int>[];
+
+      final index = binarySearch(numbers, key: 1, lowerRange: 0, upperRange: numbers.length);
+
+      expect(index, isNull);
+    });
   });
 
-  test('Searching for int value not included in the list', () {
-    final numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67];
+  group('double', () {
+    test('Searching for value included in the list', () {
+      final numbers = [2.0, 3.0, 5.0, 7.0, 11.0, 13.0, 17.0, 19.0, 23.0, 29.0, 31.0, 37.0, 41.0, 43.0, 47.0, 53.0, 59.0, 61.0, 67.0];
 
-    final index = binarySearch(numbers, key: 10, lowerRange: 0, upperRange: numbers.length);
+      final index = binarySearch(numbers, key: 43, lowerRange: 0, upperRange: numbers.length);
 
-    expect(index, isNull);
-  });
-
-  test('Searching for double value included in the list', () {
-    final numbers = [2.0, 3.0, 5.0, 7.0, 11.0, 13.0, 17.0, 19.0, 23.0, 29.0, 31.0, 37.0, 41.0, 43.0, 47.0, 53.0, 59.0, 61.0, 67.0];
-
-    final index = binarySearch(numbers, key: 43, lowerRange: 0, upperRange: numbers.length);
-
-    expect(index, equals(13));
-  });
-
-  test('Searching for int value in one-item list', () {
-    final numbers = [1];
-
-    final index = binarySearch(numbers, key: 1, lowerRange: 0, upperRange: numbers.length);
-
-    expect(index, equals(0));
-  });
-
-  test('Searching for int value in an empty list', () {
-    final numbers = <int>[];
-
-    final index = binarySearch(numbers, key: 1, lowerRange: 0, upperRange: numbers.length);
-
-    expect(index, isNull);
+      expect(index, equals(13));
+    });
   });
 
 }
