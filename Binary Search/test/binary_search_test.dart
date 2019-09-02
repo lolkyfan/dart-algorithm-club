1dd993b5b22f2a60af5c7398c2a1b925488f17c7
Jakub Sekerka <sekerka.jakub@gmail.com>
2019-09-02 15:58:59 +0200

    Update binary search tests

--- Binary Search/test/binary_search_test.dart
+++ Binary Search/test/binary_search_test.dart
@@ -3,19 +3,18 @@ import 'package:test/test.dart';
 import '../lib/main.dart';
 
 void main() {
+
+  final intNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67];
+
   group('Integer values', () {
     test('Searching for value included in the list', () {
-      final numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67];
-
-      final positionInTheList = binarySearch(numbers, key: 43, lowerRange: 0, upperRange: numbers.length);
+      final positionInTheList = binarySearch(intNumbers, key: 43, lowerRange: 0, upperRange: intNumbers.length);
 
       expect(positionInTheList, equals(13));
     });
 
     test('Searching for value not included in the list', () {
-      final numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67];
-
-      final index = binarySearch(numbers, key: 10, lowerRange: 0, upperRange: numbers.length);
+      final index = binarySearch(intNumbers, key: 10, lowerRange: 0, upperRange: intNumbers.length);
 
       expect(index, isNull);
     });
