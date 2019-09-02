a117ebbe3b1fdb7d53e9c6284b2f18a004601878
Jakub Sekerka <sekerka.jakub@gmail.com>
2019-09-02 15:47:04 +0200

    Add Array2D class

--- Array2D/lib/main.dart
+++ Array2D/lib/main.dart
@@ -1,5 +1,38 @@
+/// Fixed-length two dimensional array with a [columns] x [rows] size.
+class Array2D<T> {
+  /// Number of columns in the array
+  final int columns;
+  /// Number of rows in the array
+  final int rows;
+  List<T> _array;
 
+  /// Returns a 2D array filled with [initialValue] of [columns] x [rows] size.
+  Array2D(this.columns, this.rows, T initialValue) {
+    assert(columns > 0, 'Columns must be a positive number.');
+    assert(rows > 0, 'Rows must be a positive number.');
+
+    _array = List.generate(columns*rows, (_) => initialValue);
+  }
+
+  /// Returns an item at a given position. Throws [RangeError] if at least
+  /// one of indexes is out of range.
+  T item(int column, int row) {
+    RangeError.checkValueInInterval(column, 0, columns);
+    RangeError.checkValueInInterval(row, 0, rows);
+
+    return _array[row*columns + column];
+  }
+
+  /// Sets an item at a given position. Throws [RangeError] if at least
+  /// one of indexes is out of range.
+  void set(int column, int row, T value) {
+    RangeError.checkValueInInterval(column, 0, columns);
+    RangeError.checkValueInInterval(row, 0, rows);
+
+    _array[row*columns + column] = value;
+  }
+}
 
 void main() {
     print('Array 2D implementation in Dart. See tests for example usage.');
-  }
+}
--- Array2D/test/array_2d_test.dart
+++ Array2D/test/array_2d_test.dart
@@ -1,7 +1,29 @@
 import 'package:test/test.dart';
 
+import '../lib/main.dart';
+
 void main() {
-  test('Array 2D', () {
-    
+  test('Creating 2D array and getting items', () {
+    // Creating pre-filled array of zeros
+    final array2d = Array2D(2, 3, 0);
+    expect(array2d.item(0,0), equals(0));
+    expect(array2d.item(1,1), equals(0));
+    expect(array2d.item(1,2), equals(0));
+  });
+
+  test('Setting item in a 2D array', () {
+    // Creating pre-filled array of zeros
+    final array2d = Array2D(2, 3, 0);
+    array2d.set(1, 1, 3);
+
+    expect(array2d.item(1,1), equals(3));
+  });
+
+  test('Out of range throws an error', () {
+    // Creating pre-filled array of zeros
+    final array2d = Array2D(2, 3, 0);
+
+    expect(() => array2d.item(3, 4), throwsRangeError);
+    expect(() => array2d.set(3, 4, 12), throwsRangeError);
   });
 }
